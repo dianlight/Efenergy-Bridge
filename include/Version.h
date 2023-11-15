f68967e67a4fbeb7e70d3f07ae929312c7f1734a
dianlight <lucio.tarantino@gmail.com>
2023-11-15 15:26:09 +0100

    Version Bump

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.3.18"
+        #define VERSION "0.3.19"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-11-14 14:10:16.445224"
+        #define BUILD_TIMESTAMP "2023-11-15 08:29:50.282539"
     #endif
     
